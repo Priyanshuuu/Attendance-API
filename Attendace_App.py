631249df9c1c9bcc38fe6e8195ede42e97d4d4ca
Priyanshu Pandey <41360062+Priyanshuuu@users.noreply.github.com>
2020-03-02 01:52:09 +0530

    Update Attendace_App.py

--- Attendace_App.py
+++ Attendace_App.py
@@ -5,7 +5,7 @@ import time
 from tqdm import tqdm_notebook as tqdm
 
 def calc(username,password):
-    #Add your web browser header details, mine was Mozilla.
+   
     header = {
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0'
     }
